2cae3dec6c079c40411482931c8f944e3e6302ed
Timberwoof <gitwoof@timberwoof.com>
2023-08-28 17:35:31 -0700

    renamed test_m9301 and added B variant

---
+++ test_M9301.py
@@ -0,0 +1,27 @@
+import pytest
+from pdp11 import pdp11CPU
+from pdp11 import pdp11Run
+from pdp11Boot import pdp11Boot
+class TestClass():
+
+    def test_M9301_YA(self):
+        """The M9301-YA is designed specifically for PDP-ll/04 and PDP-ll/34 OEM systems."""
+        print('test_M9301-YA')
+        pdp11 = pdp11CPU()
+        boot = pdp11Boot(pdp11.reg, pdp11.ram)
+        Boot0 = pdp11.boot.read_PDP11_assembly_file('source/M9301-YA.txt')
+        pdp11.reg.set_pc(0o165000, "test_M9301-YA") # 0o165000
+        pdp11.ram.dump(0o165000, 0o165007)
+        run = pdp11Run(pdp11)
+        run.runInTerminal()
+
+    def test_M9301_YB(self):
+        """The M9301-YB is designed specifically for PDP-ll/04 and PDP-ll/34 end user systems."""
+        print('test_M9301-YB')
+        pdp11 = pdp11CPU()
+        boot = pdp11Boot(pdp11.reg, pdp11.ram)
+        Boot0 = pdp11.boot.read_PDP11_assembly_file('source/M9301-YA.txt')
+        pdp11.reg.set_pc(0o165000, "test_M9301-YB") # 0o165000
+        pdp11.ram.dump(0o165000, 0o165007)
+        run = pdp11Run(pdp11)
+        run.runInTerminal()
